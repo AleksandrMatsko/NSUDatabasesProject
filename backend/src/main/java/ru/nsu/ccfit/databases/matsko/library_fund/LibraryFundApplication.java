2a89372cc9620ada5a76ade8da2f01051c2980dc
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-04-24 20:36:15 +0700

    initial commit of application

---
+++ backend/src/main/java/ru/nsu/ccfit/databases/matsko/library_fund/LibraryFundApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.databases.matsko.library_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryFundApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/nsu/ccfit/databases/matsko/library_fund/LibraryFundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.databases.matsko.library_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
