e70ab256735eddb0ec618a596553278513f3c456
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-05-23 19:33:38 +0700

    feat: start main screen

---
+++ frontend/library_fund/lib/utils/constats.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const appLargeTextSize = 30.0;
+const appMediumTextSize = 22.0;
+const appBodyTextSize = 20.0;
+
+const appPrimaryColor = Color.fromARGB(255, 255, 85, 142);
+const appSecondaryColor = Color.fromARGB(255, 117, 3, 137);
+const appBackgroundColor = Colors.orange;
+
+const appFontFamily = "Roboto";
---
+++ frontend/library_fund/lib/utils/theme.dart
@@ -0,0 +1,36 @@
+import "package:flutter/material.dart";
+
+import "./constats.dart";
+
+ThemeData basicTheme() => ThemeData(
+      brightness: Brightness.light,
+      primaryColor: appPrimaryColor,
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontFamily: appFontFamily,
+          fontSize: appMediumTextSize,
+          fontWeight: FontWeight.w800,
+          color: Colors.black,
+        ),
+        headlineMedium: TextStyle(
+            fontFamily: appFontFamily,
+            fontSize: appMediumTextSize,
+            fontWeight: FontWeight.w100,
+            color: appSecondaryColor),
+        bodyLarge: TextStyle(
+          fontFamily: appFontFamily,
+          fontSize: appBodyTextSize,
+          color: Color.fromARGB(255, 58, 55, 55),
+        ),
+      ),
+      buttonTheme: const ButtonThemeData(
+        height: 80,
+        buttonColor: appPrimaryColor,
+        textTheme: ButtonTextTheme.accent,
+      ),
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: appPrimaryColor,
+          primary: appPrimaryColor,
+          secondary: appSecondaryColor,
+          background: appBackgroundColor),
+    );
