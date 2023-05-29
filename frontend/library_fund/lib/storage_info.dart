c4bf10299af9deb3a6973e1ef4e7680b1c4e6d7c
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-05-29 20:59:18 +0700

    feat: creating si, fix: table display

--- frontend/library_fund/lib/storage_info.dart
+++ frontend/library_fund/lib/storage_info.dart
@@ -1,4 +1,10 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'menu.dart';
 import 'repositories/storage_info_repository.dart';
 import 'repositories/dtos.dart';
@@ -46,10 +52,286 @@ class SIOptionsScreen extends StatelessWidget {
                 child: const Text(
                     "Выдать список инвентарных номеров и названий из библиотечного фонда, в которых содержатся произведения указанного автора")),
             OutlinedButton(
-                onPressed: () {},
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, "/si/addOne"),
                 child: const Text("Добавить новый экземпляр")),
           ],
         ));
+  }
+}
+
+class SIAddScreen extends StatefulWidget {
+  const SIAddScreen({super.key});
+
+  @override
+  State<SIAddScreen> createState() => _SIAddScreenState();
+}
+
+class _SIAddScreenState extends State<SIAddScreen> {
+  final _siRepository = SIRepository();
+  late Future<bool> _success;
+  var _state = RequestWithParamsState.askingUser;
+  late String _errorMessage;
+  String? _bookId;
+  String? _hallId;
+  String? _bookcase;
+  String? _shelf;
+  bool? _availableIssue = true;
+  String? _durationIssue;
+  DateTime? _dateReceipt;
+  DateTime? _dateDispose;
+
+  void _userReady() {
+    setState(() {
+      if (_bookId == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указан id книги";
+        return;
+      }
+      if (_hallId == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указан id зала";
+        return;
+      }
+      if (_bookcase == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указан номер книжного шкафа";
+        return;
+      }
+      if (_shelf == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указан номер полки";
+        return;
+      }
+      if (_durationIssue == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указана длительность выдачи";
+        return;
+      }
+      if (_dateReceipt == null) {
+        _state = RequestWithParamsState.errorInput;
+        _errorMessage = "Не указана дата получения экземпляра";
+        return;
+      }
+
+      initializeDateFormatting();
+      DateFormat formatter = DateFormat(dateTemplate);
+      String receiptDate = formatter.format(_dateReceipt!);
+      String? disposeDate;
+      if (_dateDispose != null) {
+        disposeDate = formatter.format(_dateDispose!);
+      }
+
+      _success = _siRepository.create({
+        "bookId": int.parse(_bookId!),
+        "hallId": int.parse(_hallId!),
+        "bookcase": int.parse(_bookcase!),
+        "shelf": int.parse(_shelf!),
+        "availableIssue": _availableIssue!,
+        "durationIssue": int.parse(_durationIssue!),
+        "dateReceipt": receiptDate,
+        "dateDispose": disposeDate,
+      });
+      _state = RequestWithParamsState.showingInfo;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            "Добавление хранимого экземпляра",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (BuildContext newBuildContext) {
+                    return const Menu();
+                  }));
+                },
+                icon: const Icon(Icons.menu_rounded))
+          ],
+        ),
+        body: switch (_state) {
+          RequestWithParamsState.askingUser => ListView(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+              children: [
+                Text("id книги", style: Theme.of(context).textTheme.bodyLarge),
+                TextFormField(
+                  showCursor: true,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  onChanged: (value) {
+                    _bookId = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+                Text("id зала", style: Theme.of(context).textTheme.bodyLarge),
+                TextFormField(
+                  showCursor: true,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  onChanged: (value) {
+                    _hallId = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+                Text("Номер книжного шкафа",
+                    style: Theme.of(context).textTheme.bodyLarge),
+                TextFormField(
+                  showCursor: true,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  onChanged: (value) {
+                    _bookcase = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+                Text("Номер полки",
+                    style: Theme.of(context).textTheme.bodyLarge),
+                TextFormField(
+                  showCursor: true,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  onChanged: (value) {
+                    _shelf = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+                Text("Возможность выноса из библиотеки",
+                    style: Theme.of(context).textTheme.bodyLarge),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Checkbox(
+                      value: _availableIssue,
+                      onChanged: (value) {
+                        setState(() {
+                          _availableIssue = value;
+                        });
+                      }),
+                ),
+                Text("Длительность выдачи (в днях)",
+                    style: Theme.of(context).textTheme.bodyLarge),
+                TextFormField(
+                  showCursor: true,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  onChanged: (value) {
+                    _durationIssue = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    child: FilledButton(
+                      onPressed: () async {
+                        final DateTime? dateTime = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1990),
+                          lastDate: DateTime.now(),
+                        );
+                        if (dateTime != null) {
+                          _dateReceipt = dateTime;
+                        }
+                      },
+                      child: const Row(children: [
+                        Icon(Icons.calendar_today),
+                        Text("Задать дату получения")
+                      ]),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    child: FilledButton(
+                      onPressed: () async {
+                        final DateTime? dateTime = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1990),
+                          lastDate: DateTime.now(),
+                        );
+                        if (dateTime != null) {
+                          _dateDispose = dateTime;
+                        }
+                      },
+                      child: const Row(children: [
+                        Icon(Icons.calendar_today),
+                        Text("Задать дату списания")
+                      ]),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    child: FilledButton(
+                        onPressed: _userReady,
+                        child: const Row(
+                          children: [Text("Добавить")],
+                        ))),
+              ],
+            ),
+          RequestWithParamsState.showingInfo => FutureBuilder<bool>(
+              future: _success,
+              builder: (context, snapshot) {
+                var isReady = snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.done;
+
+                if (isReady && snapshot.data!) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: Column(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(40),
+                        child: Text("Экземпляр добавлен",
+                            style: Theme.of(context).textTheme.titleLarge),
+                      ),
+                      FilledButton(
+                          onPressed: () =>
+                              Navigator.pushReplacementNamed(context, "/si"),
+                          child: const Text("Меню экземляров")),
+                    ]),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Ошибка: ${snapshot.error?.toString()}",
+                    style: errorStyle,
+                  ));
+                }
+
+                if (isReady && !snapshot.data!) {
+                  return const Center(
+                      child: Text(
+                    "Ошибка: что-то пошло не так",
+                    style: errorStyle,
+                  ));
+                }
+
+                return const Center(child: CircularProgressIndicator());
+              }),
+          RequestWithParamsState.errorInput => Center(
+              child: Text(_errorMessage, style: errorStyle),
+            )
+        });
   }
 }
 
@@ -97,11 +379,12 @@ class _SIAllScreenState extends State<SIAllScreen> {
               snapshot.connectionState == ConnectionState.done;
 
           if (isReady) {
-            return Table(
-              border: TableBorder.all(),
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: getSITableRows(context, snapshot),
-            );
+            return ListView(children: [
+              DataTable(
+                columns: getSITableColumns(context),
+                rows: getSITableRows(context, snapshot),
+              )
+            ]);
           }
 
           if (snapshot.hasError) {
@@ -115,88 +398,118 @@ class _SIAllScreenState extends State<SIAllScreen> {
   }
 }
 
-List<TableRow> getSITableRows(var context, var snapshot) {
-  List<TableRow> tmp = [
-    TableRow(children: [
-      Text(
+List<DataColumn> getSITableColumns(BuildContext context) {
+  return <DataColumn>[
+    DataColumn(
+      label: Text(
         "id экземпляра",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "id зала",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "номер шкафа",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "номер полки",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "можно ли выносить из библиотеки",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "максимальный срок выдачи (в днях)",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "дата поступления",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-      Text(
+    ),
+    DataColumn(
+      label: Text(
         "дата списания",
         style: Theme.of(context).textTheme.bodyLarge,
       ),
-    ])
+    )
   ];
-  for (var si in snapshot!.data) {
+}
+
+List<DataRow> getSITableRows(BuildContext context, var snapshot) {
+  List<DataRow> res = [];
+  for (var si in snapshot!.data!) {
     var availableIssueWord = "";
     if (si.availableIssue) {
       availableIssueWord = "да";
     } else {
       availableIssueWord = "нет";
     }
-    tmp.add(TableRow(
-      children: [
+    res.add(DataRow(cells: [
+      DataCell(
         Text(
           "${si.storedId}",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           "${si.hallId}",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           "${si.bookcase}",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           "${si.shelf}",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           availableIssueWord,
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           "${si.durationIssue}",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      DataCell(
         Text(
           si.dateReceipt,
           style: Theme.of(context).textTheme.bodyLarge,
         ),
-        Text(
-          "${si.dateDispose ?? ""}",
-          style: Theme.of(context).textTheme.bodyLarge,
-        )
-      ],
-    ));
+      ),
+      DataCell(Text(
+        "${si.dateDispose ?? ""}",
+        style: Theme.of(context).textTheme.bodyLarge,
+      )),
+    ]));
   }
-  return tmp;
+  return res;
 }
 
 class SIByTemplate extends StatefulWidget {
@@ -293,12 +606,12 @@ class _SIByTemplateState extends State<SIByTemplate> {
                       snapshot.connectionState == ConnectionState.done;
 
                   if (isReady) {
-                    return Table(
-                      border: TableBorder.all(),
-                      defaultVerticalAlignment:
-                          TableCellVerticalAlignment.middle,
-                      children: getSITableRows(context, snapshot),
-                    );
+                    return ListView(children: [
+                      DataTable(
+                        columns: getSITableColumns(context),
+                        rows: getSITableRows(context, snapshot),
+                      )
+                    ]);
                   }
 
                   if (snapshot.hasError) {
